56b319b0a5b020ca13a3b338855732d93762ed89
Noah Noel Peeters <noah.peeters@icloud.com>
2017-10-28 21:46:20 +0200

    Initial Commit

---
+++ DevrantAvatar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DevrantAvatar
+//
+//  Created by Noah Peeters on 10/28/17.
+//  Copyright © 2017 Noah Peeters. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DevrantAvatar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DevrantAvatar
+//
+//  Created by Noah Peeters on 10/28/17.
+//  Copyright © 2017 Noah Peeters. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
